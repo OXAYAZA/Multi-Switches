e3d2639ad920a8751962c2e8bdab1ffc9edf6d63
oxayaza <gashishmarihuanovich@gmail.com>
2019-06-12 01:06:47 +0300

    UPD: v2.1.0

--- dev/MultiSwitches.js
+++ dev/MultiSwitches.js
@@ -1,6 +1,6 @@
 /**
  * @module       MultiSwitches
- * @version      2.0.0
+ * @version      2.1.0
  * @author       OXAYAZA {@link https://github.com/OXAYAZA}
  * @license      CC BY-SA 4.0 {@link https://creativecommons.org/licenses/by-sa/4.0/}
  * @see          {@link https://codepen.io/OXAYAZA/pen/eRbYjV}
@@ -8,34 +8,41 @@
  * @see          {@link https://oxayaza.page.link/linkedin}
  * @description  Allows you to use multiple switches to switch class on the target,
  *               itself and other switches linked to the same target.
- * @todo comments
  */
 
 function MultiSwitch( options ) {
 	/**
-	 * @desc Returns random string
-	 * @param {number} length - string length
+	 * Switch prototype
+	 * @param {object} options
+	 * @param {Element} options.node - элемент из которого будет создан переключатель
+	 * @param {NodeList|string} options.targets - переклюаемые элементы
+	 * @param {boolean} [options.state] - начальное состояние переключателя
+	 * @param {string} [options.class] - переключаемый класс
+	 * @param {string} [options.event] - событие для переключения
+	 * @param {NodeList|string} [options.scope] - 
+	 * @param {NodeList|string} [options.isolate] - 
+	 * @returns {Switch}
+	 * @constructor
 	 */
-	function randomStr( length ) {
-		let str = '';
-		for ( let n = 0; n < length; n++ ) str += String.fromCharCode( 97 + Math.random() * 25 );
-		return str;
-	}
-
-	// Switcher prototype
 	function Switch( options ) {
-		this.id = randomStr( 8 );
+		// Check passed options
+		if( !( options.node instanceof Element ) ) {
+			throw new Error( 'Switch element is required' );
+		}
+		if( !( options.targets instanceof NodeList ) && typeof( options.targets ) !== 'string') {
+			throw new Error( 'Target is required' );
+		}
 
-		// Merge this, defaults & options
+		// Merging this, defaults and options
 		for ( let key in Switch.defaults ) this[ key ] = Switch.defaults[ key ];
 		for ( let key in options ) this[ key ] = options[ key ];
 
-		// Get node lists
+		// Get node lists if strings are passed
 		[ 'targets', 'scope', 'isolate' ].forEach( ( function ( key ) {
 			this[ key ] = Switch.procSelector( this[ key ] );
 		}).bind( this ));
 
-		// Link node and switch
+		// Link the switch instance to the element
 		this.node.multiSwitch = this;
 
 		// Init targets
@@ -46,20 +53,39 @@ function MultiSwitch( options ) {
 
 		// Assign an switch event handler
 		this.node.addEventListener( this.event, ( function() {
-			console.log( `SWITCH [${this.id}]: CLICK`, this.state );
 			this.changeState();
 		}).bind( this ));
 
 		// Assign an emitter event handler
 		this.node.addEventListener( `switch:${this.class}`, ( function( event ) {
-			console.log( `SWITCH [${this.id}]: GET [${event.emitter.multiSwitchTarget.id}]`, event.emitter.multiSwitchTarget.groups[ this.class ].state );
 			if( event.emitter.multiSwitchTarget.groups[ this.class ].state !== this.state ) this.changeState( event.emitter.multiSwitchTarget.groups[ this.class ].state );
 		}).bind( this ));
 
-		console.log( '%c[MultiSwitch]', 'color: yellow', this.node );
+		// Assign an switch event handler with scope check
+		if ( this.scope && this.scope.length ) {
+			document.addEventListener( this.event, ( function ( event ) {
+				if( !this.checkScope( event.target ) && this.state ) this.changeState();
+			}).bind( this ));
+		}
+
+		// Assign an switch event handler with isolation check
+		if ( this.isolate && this.isolate.length ) {
+			document.addEventListener( this.event, ( function ( event ) {
+				if( this.checkIsolate( event.target ) && this.state ) this.changeState();
+			}).bind( this ));
+		}
+
+		// Set initial state
+		this.changeState( this.state );
+
 		return this;
 	}
 
+	/**
+	 * Default switch parameters
+	 * @type {object}
+	 * @static
+	 */
 	Switch.defaults = {
 		node: null,
 		state: false,
@@ -70,11 +96,21 @@ function MultiSwitch( options ) {
 		isolate: null
 	};
 
+	/**
+	 * Returns NodeList if selector string is passed
+	 * @param {NodeList|string} selector
+	 * @returns {NodeList}
+	 * @static
+	 */
 	Switch.procSelector = function ( selector ) {
 		if ( typeof( selector ) === 'string' ) return document.querySelectorAll( selector );
 		return selector;
 	};
 
+	/**
+	 * Changing the state of the switch to the specified or opposite of the initial
+	 * @param {boolean} [state]
+	 */
 	Switch.prototype.changeState = function ( state ) {
 		if ( typeof( state ) !== 'boolean' ) this.state = !this.state;
 		else this.state = state;
@@ -82,25 +118,67 @@ function MultiSwitch( options ) {
 		if ( this.state ) this.node.classList.add( this.class );
 		else this.node.classList.remove( this.class );
 
+		// Dispatching switching events to all targets
 		this.targets.forEach( ( function( target ) {
 			let event = new CustomEvent( `switch:${this.class}` );
 			event.emitter = this.node;
-			console.log( `SWITCH [${this.id}]: SEND`, this.state );
 			target.dispatchEvent( event );
 		}).bind( this ));
 	};
 
-	// Target Prototype
+	/**
+	 * Check whether the element in the switch or in one of the specified areas.
+	 * @param {Element} node
+	 * @returns {boolean}
+	 */
+	Switch.prototype.checkScope = function ( node ) {
+		if( this.node.contains( node ) ) return true;
+
+		for ( let i = 0; i < this.scope.length; i++ ) {
+			if( this.scope[i].contains( node ) ) return true;
+		}
+
+		return false;
+	};
+
+	/**
+	 * Check whether the element is one of the isolated elements.
+	 * @param {Element} node
+	 * @returns {boolean}
+	 */
+	Switch.prototype.checkIsolate = function ( node ) {
+		if( this.node.contains( node ) ) return false;
+
+		for ( let i = 0; i < this.isolate.length; i++ ) {
+			if( this.isolate[i].contains( node ) ) return true;
+		}
+
+		return false;
+	};
+
+	/**
+	 * Target prototype
+	 * @param {Element} node - target element
+	 * @returns {Target}
+	 * @constructor
+	 */
 	function Target( node ) {
-		this.id = randomStr( 8 );
 		this.node = node;
 		this.groups = {};
 
+		// Link the target instance to the element
 		this.node.multiSwitchTarget = this;
-		console.log( '%c[MultiSwitchTarget]', 'color: cyan', this.node );
+
 		return this;
 	}
 
+	/**
+	 * Creating or updating a group of switches linked to the target, which is based on a switchable class.
+	 * @param {object} params
+	 * @param {Element} params.node - linked switch
+	 * @param {string} params.class - switchable class of the group
+	 * @param {boolean} params.state - initial target state
+	 */
 	Target.prototype.updateGroup = function ( params ) {
 		if ( !this.groups[ params.class ] ) {
 			this.groups[ params.class ] = {
@@ -108,18 +186,22 @@ function MultiSwitch( options ) {
 				class: params.class,
 				switches: [],
 				event: ( function( event ) {
-					console.log( `TARGET [${this.id}]: GET [${event.emitter.multiSwitch.id}]`, event.emitter.multiSwitch.state );
 					if( event.emitter.multiSwitch.state !== this.state ) this.changeState( event.emitter.multiSwitch.state, event.emitter.multiSwitch.class );
 				}).bind( this )
 			};
 
+			// Assign an emitter event handler
 			this.node.addEventListener( `switch:${params.class}`, this.groups[ params.class ].event );
 		}
 
 		this.groups[ params.class ].switches.push( params.node );
-		console.log( this.groups );
 	};
 
+	/**
+	 * Changing the state of the target to the specified or opposite of the initial
+	 * @param {boolean} [state]
+	 * @param {string} className - switchable class
+	 */
 	Target.prototype.changeState = function ( state, className ) {
 		let group = this.groups[ className ];
 
@@ -129,10 +211,10 @@ function MultiSwitch( options ) {
 		if ( group.state ) this.node.classList.add( group.class );
 		else this.node.classList.remove( group.class );
 
+		// Dispatching switching events to all linked switches
 		group.switches.forEach( ( function ( node ) {
 			let event = new CustomEvent( `switch:${group.class}` );
 			event.emitter = this.node;
-			console.log( `TARGET [${this.id}]: SEND`, group.state );
 			node.dispatchEvent( event );
 		}).bind( this ));
 	};
